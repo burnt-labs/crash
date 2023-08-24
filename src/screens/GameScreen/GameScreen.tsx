7fb59621ae3fe4d287d74885fd153bdb00fe0963
Denis Krasovskyi <denis.krasovskyi@gmail.com>
2023-08-25 02:03:47 +0300

    feat: add game styles

--- src/screens/GameScreen/GameScreen.tsx
+++ src/screens/GameScreen/GameScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './GameScreen.module.scss';
 
 interface GameScreenProps {
   className?: string;
 }
 
 export const GameScreen: React.FC<GameScreenProps> = () => {
-  return <section>Game Screen</section>;
+  return <section className={styles.root}>Game Screen</section>;
 };
