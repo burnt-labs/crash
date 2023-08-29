09d4d0b9af92edfb1736d77e4d2a1e543d4d1e9f
Denis Krasovskyi <denis.krasovskyi@gmail.com>
2023-08-29 17:05:12 +0300

    feat: update configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -19,7 +19,7 @@ export const appConfig: AppConfig = {
   txSpamDuration: parseInt(process.env.NEXT_PUBLIC_TX_SPAM_DURATION as string),
   txSpamInterval: 50,
   numberOfSigners: 2,
-  fundingAmount: '500000uxion',
+  fundingAmount: '1000000uxion',
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL as string,
   // mnemonics: [
   //   'fatigue later mass devote case over home return oyster guess warrior correct much tip torch robot acid coast response supply emerge antique replace faint',
