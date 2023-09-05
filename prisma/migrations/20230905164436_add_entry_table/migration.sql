ea9140f0810a8cda324c06a4921737fc0d3b5b50
justin <328965+justinbarry@users.noreply.github.com>
2023-09-05 09:45:19 -0700

    build: migration for entry table

---
+++ prisma/migrations/20230905164436_add_entry_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Entry" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "score" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Entry_pkey" PRIMARY KEY ("id")
+);
